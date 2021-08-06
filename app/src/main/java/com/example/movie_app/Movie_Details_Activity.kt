c6a22ebf7a5d0fc45a134cec6a6d1004ce6e2fbf
rokydas <droky136@gmail.com>
2021-08-06 23:17:39 +0600

    details page done by reall data from api

--- app/src/main/java/com/example/movie_app/Movie_Details_Activity.kt
+++ app/src/main/java/com/example/movie_app/Movie_Details_Activity.kt
@@ -3,17 +3,35 @@ package com.example.movie_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.movie_details_activity.*
+import android.graphics.Typeface
+import android.view.View
+
 
 class Movie_Details_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_details_activity)
 
+        fillStar.visibility = View.GONE
+
         val movie = intent.getParcelableExtra<movie_model>("movie")
+        val imgURl = "https://image.tmdb.org/t/p/original/" + movie?.poster_path
 
         text.text = movie?.title
+        description.text = movie?.overview
+        popularity.text = movie?.popularity.toString()
+        release_date.text = movie?.release_date
+        movie_poster.load(imgURl)
 
+        star.setOnClickListener() { changeVisibilityOfStar(star, fillStar) }
+        fillStar.setOnClickListener() { changeVisibilityOfStar(fillStar, star) }
+    }
+
+    fun changeVisibilityOfStar(previous: View, new: View) {
+        new.visibility = View.VISIBLE
+        previous.visibility = View.GONE
     }
 }
